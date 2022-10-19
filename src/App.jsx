b989139749e923faae92c26d9dc6ad3d03d4bfa8
Guilherme Selair <guilhermeselair@gmail.com>
2022-10-19 14:02:20 -0300

    Chore: Formatted dates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import styles from "./App.module.css";
 import Post from "./components/Post";
 import Sidebar from "./components/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/guiselair.png",
+      name: "Guilherme Selair",
+      role: "Frontend developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "jane.design/doctorcare" }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/guiselair.png",
+      name: "Guilherme Selair",
+      role: "Frontend developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "jane.design/doctorcare" }
+    ],
+    publishedAt: new Date('2022-10-19 10:00:00')
+  }
+]
+
 function App() {
   return (
     <div className="App">
@@ -13,10 +44,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Guilherme Selair"
-            content="Salve Salve! Começando mais um post por aqui!"
-          />
+          {posts.map(post => (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
